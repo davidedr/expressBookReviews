42c9f8983a88acd69319d9e656f01e2fe4fcde6e
Davide DR <d.dr@libero.it>
2025-01-22 12:21:06 +0100

    Completes Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -34,7 +34,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;  
+  if (!author)
+    return res.status(404).send(`author not valid!`);
+  let found_books = [];
+  for (k in books) {
+    let b  = books[k];
+    if (b.author === author)
+      found_books.push(b);
+  }
+  if (found_books && found_books.length > 0)
+    return res.send(JSON.stringify(found_books, null, 4));
+  else
+    return res.status(404).send(`No books found w/ author: ${author}!`);
 });
 
 // Get all books based on title
