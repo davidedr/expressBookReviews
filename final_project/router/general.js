89489399850a940c376906c573982bcc4bdc0b2e
Davide DR <d.dr@libero.it>
2025-01-22 21:56:59 +0100

    Completes Tasks 11 and 12

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,33 +38,64 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   //return res.status(300).json({message: "Yet to be implemented"});
-  const isbn = req.params.isbn;
-  if (!isbn)
-    return res.status(404).send(`ISBN ${isbn} not valid!`);
-  const found_book = books[isbn];
-  if (found_book)
-    return res.send(JSON.stringify(found_book, null, 4));
-  else
-    return res.status(404).send(`Book w/ ISBN: ${isbn} not found!`);
- });
-  
+  // Taks 2
+  // const isbn = req.params.isbn;
+  // if (!isbn)
+  //   return res.status(404).send(`ISBN ${isbn} not valid!`);
+  // const found_book = books[isbn];
+  // if (found_book)
+  //   return res.send(JSON.stringify(found_book, null, 4));
+  // else
+  //   return res.status(404).send(`Book w/ ISBN: ${isbn} not found!`);
+
+  // Task 11
+  new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    if (!isbn)
+      res.status(404).send(`ISBN ${isbn} not valid!`);
+    const found_book = books[isbn];
+    if (found_book)
+      res.send(JSON.stringify(found_book, null, 4));
+    else
+      res.status(404).send(`Book w/ ISBN: ${isbn} not found!`);
+  });
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
   //return res.status(300).json({message: "Yet to be implemented"});
-  const author = req.params.author;  
-  if (!author)
-    return res.status(404).send(`author not valid!`);
-  let found_books = [];
-  for (k in books) {
-    let b  = books[k];
-    if (b.author === author)
-      found_books.push(b);
-  }
-  if (found_books && found_books.length > 0)
-    return res.send(JSON.stringify(found_books, null, 4));
-  else
-    return res.status(404).send(`No books found w/ author: ${author}!`);
+  // Task 3
+  // const author = req.params.author;  
+  // if (!author)
+  //   return res.status(404).send(`author not valid!`);
+  // let found_books = [];
+  // for (k in books) {
+  //   let b  = books[k];
+  //   if (b.author === author)
+  //     found_books.push(b);
+  // }
+  // if (found_books && found_books.length > 0)
+  //   return res.send(JSON.stringify(found_books, null, 4));
+  // else
+  //   return res.status(404).send(`No books found w/ author: ${author}!`);
+
+  // Task 12
+  new Promise((resolve, reject) => {
+    const author = req.params.author;  
+    if (!author)
+      res.status(404).send(`author not valid!`);
+    let found_books = [];
+    for (k in books) {
+      let b  = books[k];
+      if (b.author === author)
+        found_books.push(b);
+    }
+    if (found_books && found_books.length > 0)
+      res.send(JSON.stringify(found_books, null, 4));
+    else
+      res.status(404).send(`No books found w/ author: ${author}!`);
+  });
 });
 
 // Get all books based on title
